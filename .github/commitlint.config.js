d94e24b91583f090df61413168a9f05c21609ce1
joss <joss@chi.swan.ac.uk>
2024-03-26 01:10:45 +0000

    feat: add scopes to config

--- .github/commitlint.config.js
+++ .github/commitlint.config.js
@@ -1,4 +1,15 @@
+import { RuleConfigSeverity } from '@commitlint/types';
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  parserPreset: 'conventional-changelog-conventionalcommits',
+  rules: {
+    'scope-enum': [RuleConfigSeverity.Error, 'always', [
+        '',
+        'deps',
+        'controller-container',
+        'fizzbuzz-chart',
+        'fizzbuzz-crds-chart'
+    ]]
+  }
 };
