247b19d82b9470d21ea8164bde69aeb2e6e4e3c3
Rowan Dash <rowan.dash@swansea.ac.uk>
2025-11-27 15:07:43 +0000

    test: bad python

--- containers/controller/src/controller.py
+++ containers/controller/src/controller.py
@@ -4,3 +4,14 @@ import logging
 if __name__ == "__main__":
     logging.info("hello github!")
 #ghhghghhghhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+from typing import Iterable
+
+import os
+
+
+def sum_even_numbers(numbers: Iterable[int]) -> int:
+    """Given an iterable of integers, return the sum of all even numbers in the iterable."""
+    return sum(
+        num for num in numbers
+        if num % 2 == 0
+    )
