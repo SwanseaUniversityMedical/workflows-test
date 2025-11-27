170dda2de10f28890ff65dac03fb193b9f8e1989
Rowan Dash <rowan.dash@swansea.ac.uk>
2025-11-27 14:49:53 +0000

    fix: bad python

--- containers/controller/src/controller.py
+++ containers/controller/src/controller.py
@@ -3,3 +3,4 @@ import logging
 
 if __name__ == "__main__":
     logging.info("hello github!")
+#ghhghghhghhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
